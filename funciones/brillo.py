f7554b2b9b853296a981fd0978dd6d84bb53eca5
GIANCARLO ZEVALLOS LECCA <g10n_22_92@hotmail.com>
2022-04-30 20:19:03 -0500

    brightness function

---
+++ funciones/brillo.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def brillo(imagen, factor_correccion):
+  (f, c) = persona_gris.shape
+  persona_brillo = np.zeros((f, c))
+  for i in range(0, f):
+    for j in range(0, c):
+      persona_brillo[i][j] = persona_gris[i][j] + factor_correccion
+      if persona_brillo[i][j] > 255:
+        persona_brillo[i][j] = 255
+      elif persona_brillo[i][j] < 0:
+        persona_brillo[i][j] = 0
+  return persona_brillo.astype(np.uint8)

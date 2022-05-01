812959df6c993bbcb431e8e2124248fe7c98e6d3
giancarlo92 <31825844+giancarlo92@users.noreply.github.com>
2022-04-30 20:32:45 -0500

    Create contraste.py
    contraste

---
+++ funciones/contraste.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def contraste(imagen):
+  (f, c) = imagen.shape
+  persona_contraste = np.zeros((f, c))
+  maximo = imagen.max()
+  minimo = imagen.min()
+  for i in range(0, f):
+    for j in range(0, c):
+      pixel_actual = imagen[i][j]
+      persona_contraste[i][j] = round(((pixel_actual - minimo) / (maximo - minimo)) * 255)
+      persona_contraste[i][j] = persona_contraste[i][j] + 30
+      if persona_contraste[i][j] > 255:
+        persona_contraste[i][j] = 255
+      elif persona_contraste[i][j] < 0:
+        persona_contraste[i][j] = 0
+  return persona_contraste.astype(np.uint8)

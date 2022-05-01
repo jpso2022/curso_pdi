94cd1b0a233e8f4f5813db95207dc8830c41977a
giancarlo92 <31825844+giancarlo92@users.noreply.github.com>
2022-04-30 20:27:18 -0500

    Update brillo.py
    change

--- funciones/brillo.py
+++ funciones/brillo.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def brillo(imagen, factor_correccion):
-  (f, c) = persona_gris.shape
+  (f, c) = imagen.shape
   persona_brillo = np.zeros((f, c))
   for i in range(0, f):
     for j in range(0, c):
-      persona_brillo[i][j] = persona_gris[i][j] + factor_correccion
+      persona_brillo[i][j] = imagen[i][j] + factor_correccion
       if persona_brillo[i][j] > 255:
         persona_brillo[i][j] = 255
       elif persona_brillo[i][j] < 0:

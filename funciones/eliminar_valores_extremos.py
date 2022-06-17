2a6ce66a8340312f5297a553b88336dc79ae7bae
giancarlo92 <31825844+giancarlo92@users.noreply.github.com>
2022-06-17 16:59:16 -0500

    r

--- funciones/eliminar_valores_extremos.py
+++ funciones/eliminar_valores_extremos.py
@@ -7,7 +7,7 @@ def eliminar_valores_extremos(binario, centroide, umbral):
     for i in range(0, f):
         for j in range(0, c):
             if binario[i][j] == 1:
-                SE = math.sqrt((i-centroide[0])**2 + (j-centroide[1])**2)
+                SE = math.sqrt((i-centroide[1])**2 + (j-centroide[0])**2)
                 if SE >= umbral:
                     binario[i][j] = 0
     return binario

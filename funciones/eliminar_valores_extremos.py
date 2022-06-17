f3df405fb706897cc836b570dc80d5a2f9dde9f2
giancarlo92 <31825844+giancarlo92@users.noreply.github.com>
2022-06-17 16:52:56 -0500

    f

--- funciones/eliminar_valores_extremos.py
+++ funciones/eliminar_valores_extremos.py
@@ -2,6 +2,7 @@ import math
 
 def eliminar_valores_extremos(binario, centroide, umbral):
     [f, c] = binario.shape
+    resultado = np.zeros(binario.shape)
     for i in range(0, f):
         for j in range(0, c):
             if binario[i][j] == 1:

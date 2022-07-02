bb2b63a4024ffc880c0f73b3076914405b535a25
giancarlo92 <31825844+giancarlo92@users.noreply.github.com>
2022-07-02 15:00:07 -0500

    color2regionbinario

---
+++ funciones/color2regionbinario.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def color2regionbinario(imagen, imagen_procesada):
+    imagen_rgb = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
+    Rojo = imagen_rgb[:,:,0]
+    Verde = imagen_rgb[:,:,1]
+    Azul = imagen_rgb[:,:,2]
+    imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    negativo = imagen_procesada==0
+    Rojo[negativo]  = imagen_gris[negativo]
+    Verde[negativo] = imagen_gris[negativo]
+    Azul[negativo]  = imagen_gris[negativo]
+    resultado = np.zeros(imagen.shape, np.uint8)
+    resultado[:,:,0] = Rojo
+    resultado[:,:,1] = Verde
+    resultado[:,:,2] = Azul
+    return resultado
